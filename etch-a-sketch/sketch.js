b1b004b9a59b1605e4ce5f113671f88b6cb6595a
william1755 <151338379+william1755@users.noreply.github.com>
2023-12-30 06:22:04 -0800

    working etch-a-sketch! version 1.0

--- etch-a-sketch/sketch.js
+++ etch-a-sketch/sketch.js
@@ -2,11 +2,43 @@ let container = document.createElement('div');
 document.body.appendChild(container);
 container.id = 'daddyDiv'
 
-for (i = 0; i < 16; i++) {
-    let divs = document.createElement('div');
-    divs.id = 'childDiv'
-    container.appendChild(divs);
+let squarePrompt = document.getElementById('squareQuery');
+squarePrompt.addEventListener('click', () => {
+    updateGrid();
+})
 
+
+
+function updateGrid() {
+    let squareValue = prompt('How many squares?')
+    console.log(squareValue)
+
+    function calculateSquare() {
+        let square = 500;
+        let squareSize = square / squareValue;
+        return squareSize
+    }
+
+    squareSizeReturn = calculateSquare();
+
+    console.log(squareSizeReturn)
+
+    let squareSizeInPx = squareSizeReturn.toString();
+
+    console.log(typeof(squareSizeInPx));
+
+    for (i = 0; i < squareValue * squareValue; i++) {
+        let divs = document.createElement('div');
+        divs.classList.add('childDiv')
+        container.appendChild(divs);    
+        divs.style.width = squareSizeInPx + 'px';
+        divs.style.height = squareSizeInPx + 'px';
+        
+
+ 
+
+        divs.addEventListener('mouseover', () => {
+            divs.classList.add('bgChanged')
+        });
+    }
 }
-
-
